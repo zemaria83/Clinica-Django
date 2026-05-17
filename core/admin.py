4e2b5eebd1ffb5b9c65db9efd0a33a741e2e2cba
zemaria83 <ze.maria83@gmail.com>
2026-05-17 14:06:10 +0100

    Configuracao base de dados mysql e admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Paciente, Consulta
 
-# Register your models here.
+@admin.register(Paciente)
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'data_nascimento', 'email', 'telefone']
+    search_fields = ['nome', 'email']
+
+@admin.register(Consulta)
+class ConsultaAdmin(admin.ModelAdmin):
+    list_display = ['paciente', 'data', 'motivo', 'estado']
+    list_filter = ['estado']
